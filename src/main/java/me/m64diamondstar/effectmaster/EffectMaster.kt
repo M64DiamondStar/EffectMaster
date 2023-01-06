2b643fd9ddf2a51c237bb709f807e70cbac729e5
M64DiamondStar <59317305+M64DiamondStar@users.noreply.github.com>
2023-01-06 22:06:51 +0100

    Initial commit

---
+++ src/main/java/me/m64diamondstar/effectmaster/EffectMaster.kt
@@ -0,0 +1,15 @@
+package me.m64diamondstar.effectmaster
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class EffectMaster : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+
+}
