0f214bbd5023637d0cc5221d7992d1fdae33c816
M64DiamondStar <59317305+M64DiamondStar@users.noreply.github.com>
2023-01-07 20:46:16 +0100

    Check if is loaded before usage

--- src/main/java/me/m64diamondstar/effectmaster/shows/type/Animatronic.kt
+++ src/main/java/me/m64diamondstar/effectmaster/shows/type/Animatronic.kt
@@ -1,11 +1,18 @@
 package me.m64diamondstar.effectmaster.shows.type
 
+import me.m64diamondstar.effectmaster.EffectMaster
 import me.m64diamondstar.effectmaster.shows.utils.EffectType
 import me.m64diamondstar.effectmaster.shows.utils.Show
 
-class Animatronic(show: Show, id: Int): EffectType(show, id) {
+class Animatronic(show: Show, private val id: Int): EffectType(show, id) {
 
     override fun execute() {
+        if(!EffectMaster.isAnimatronicsLoaded){
+            EffectMaster.plugin.logger.warning("The show \"${getShow().getName()}\" in category \"${getShow().getCategory()}\" " +
+                    "tried to play an Animatronic at ID $id while that plugin is not enabled. Please add the plugin to the server or remove the effect.")
+            return
+        }
+
         val name = getSection().getString("Name")!!
         val animatronic = me.thundertnt33.animatronics.api.Animatronic(name)
         animatronic.start()
--- src/main/java/me/m64diamondstar/effectmaster/shows/type/AnimatronicGroup.kt
+++ src/main/java/me/m64diamondstar/effectmaster/shows/type/AnimatronicGroup.kt
@@ -1,12 +1,19 @@
 package me.m64diamondstar.effectmaster.shows.type
 
+import me.m64diamondstar.effectmaster.EffectMaster
 import me.m64diamondstar.effectmaster.shows.utils.EffectType
 import me.m64diamondstar.effectmaster.shows.utils.Show
 import me.thundertnt33.animatronics.api.Group
 
-class AnimatronicGroup(show: Show, id: Int) : EffectType(show, id) {
+class AnimatronicGroup(show: Show, private val id: Int) : EffectType(show, id) {
 
     override fun execute() {
+        if(!EffectMaster.isAnimatronicsLoaded){
+            EffectMaster.plugin.logger.warning("The show \"${getShow().getName()}\" in category \"${getShow().getCategory()}\" " +
+                    "tried to play an Animatronic Group at ID $id while that plugin is not enabled. Please add the plugin to the server or remove the effect.")
+            return
+        }
+
         val name = getSection().getString("Name")!!
         val group = Group(name)
         group.play()
