97b9898638e03fdfb47ba6744a77d86d4ee328e0
M64DiamondStar <59317305+M64DiamondStar@users.noreply.github.com>
2023-02-14 20:28:34 +0100

    Added try catch and fixed speed doubles not working

--- src/main/java/me/m64diamondstar/effectmaster/shows/type/FountainLine.kt
+++ src/main/java/me/m64diamondstar/effectmaster/shows/type/FountainLine.kt
@@ -15,76 +15,93 @@ import org.bukkit.entity.Player
 import org.bukkit.scheduler.BukkitRunnable
 import org.bukkit.util.Vector
 
-class FountainLine(effectShow: EffectShow, id: Int) : Effect(effectShow, id) {
+class FountainLine(effectShow: EffectShow, private val id: Int) : Effect(effectShow, id) {
 
     override fun execute(players: List<Player>?) {
-        val fromLocation = LocationUtils.getLocationFromString(getSection().getString("FromLocation")!!) ?: return
-        val toLocation = LocationUtils.getLocationFromString(getSection().getString("ToLocation")!!) ?: return
-        val material = if (getSection().get("Block") != null) Material.valueOf(getSection().getString("Block")!!.uppercase()) else Material.STONE
-        val velocity =
-            if (getSection().get("Velocity") != null)
-                if(LocationUtils.getVectorFromString(getSection().getString("Velocity")!!) != null)
-                    LocationUtils.getVectorFromString(getSection().getString("Velocity")!!)!!
+
+        try {
+            val fromLocation = LocationUtils.getLocationFromString(getSection().getString("FromLocation")!!) ?: return
+            val toLocation = LocationUtils.getLocationFromString(getSection().getString("ToLocation")!!) ?: return
+            val material = if (getSection().get("Block") != null) Material.valueOf(
+                getSection().getString("Block")!!.uppercase()
+            ) else Material.STONE
+            val velocity =
+                if (getSection().get("Velocity") != null)
+                    if (LocationUtils.getVectorFromString(getSection().getString("Velocity")!!) != null)
+                        LocationUtils.getVectorFromString(getSection().getString("Velocity")!!)!!
+                    else Vector(0.0, 0.0, 0.0)
                 else Vector(0.0, 0.0, 0.0)
-            else Vector(0.0, 0.0, 0.0)
-        val randomizer = if (getSection().get("Randomizer") != null) getSection().getDouble("Randomizer") / 10 else 0.0
-        val speed = if (getSection().get("Speed") != null) getSection().getInt("Speed") * 0.05 else 0.05
+            val randomizer =
+                if (getSection().get("Randomizer") != null) getSection().getDouble("Randomizer") / 10 else 0.0
+            val speed = if (getSection().get("Speed") != null) getSection().getDouble("Speed") * 0.05 else 0.05
 
-        val moveX: Double = (toLocation.x - fromLocation.x) / speed
-        val moveY: Double = (toLocation.y - fromLocation.y) / speed
-        val moveZ: Double = (toLocation.z - fromLocation.z) / speed
-
-        var nx = moveX
-        var ny = moveY
-        var nz = moveZ
-        if (nx < 0) nx = -nx
-        if (ny < 0) ny = -ny
-        if (nz < 0) nz = -nz
-
-        var move = nx
-        if (ny > nx && ny > nz) move = ny
-        if (nz > ny && nz > nx) move = nz
-
-        val x: Double = moveX / move / 20.0 * (speed * 20.0)
-        val y: Double = moveY / move / 20.0 * (speed * 20.0)
-        val z: Double = moveZ / move / 20.0 * (speed * 20.0)
-
-        val finalMove = move
-
-        object: BukkitRunnable(){
-            var c = 0
-            var location: Location = fromLocation
-            override fun run() {
-                if (c > finalMove) {
-                    cancel()
-                    return
-                }
-
-                val fallingBlock = location.world!!.spawnFallingBlock(location, material.createBlockData())
-                fallingBlock.dropItem = false
-
-                if(randomizer != 0.0)
-                    fallingBlock.velocity = Vector(velocity.x + Math.random() * (randomizer * 2) - randomizer,
-                        velocity.y + Math.random() * (randomizer * 2) - randomizer / 3,
-                        velocity.z + Math.random() * (randomizer * 2) - randomizer)
-                else
-                    fallingBlock.velocity = velocity
-
-                ShowUtils.addFallingBlock(fallingBlock)
-
-                if(players != null && EffectMaster.isProtocolLibLoaded)
-                    for(player in Bukkit.getOnlinePlayers()){
-                        if(!players.contains(player)){
-                            val protocolManager = ProtocolLibrary.getProtocolManager()
-                            val removePacket = PacketContainer(PacketType.Play.Server.ENTITY_DESTROY)
-                            removePacket.intLists.write(0, listOf(fallingBlock.entityId))
-                            protocolManager.sendServerPacket(player, removePacket)
-                        }
-                    }
-                location.add(x, y, z)
-                c++
+            if(speed <= 0){
+                EffectMaster.plugin.logger.warning("Couldn't play effect with ID $id from ${getShow().getName()} in category ${getShow().getCategory()}.")
+                Bukkit.getLogger().warning("The speed has to be greater than 0!")
+                return
             }
-        }.runTaskTimer(EffectMaster.plugin, 0L, 1L)
+
+            val moveX: Double = (toLocation.x - fromLocation.x) / speed
+            val moveY: Double = (toLocation.y - fromLocation.y) / speed
+            val moveZ: Double = (toLocation.z - fromLocation.z) / speed
+
+            var nx = moveX
+            var ny = moveY
+            var nz = moveZ
+            if (nx < 0) nx = -nx
+            if (ny < 0) ny = -ny
+            if (nz < 0) nz = -nz
+
+            var move = nx
+            if (ny > nx && ny > nz) move = ny
+            if (nz > ny && nz > nx) move = nz
+
+            val x: Double = moveX / move / 20.0 * (speed * 20.0)
+            val y: Double = moveY / move / 20.0 * (speed * 20.0)
+            val z: Double = moveZ / move / 20.0 * (speed * 20.0)
+
+            val finalMove = move
+
+            object : BukkitRunnable() {
+                var c = 0
+                var location: Location = fromLocation
+                override fun run() {
+                    if (c > finalMove) {
+                        cancel()
+                        return
+                    }
+
+                    val fallingBlock = location.world!!.spawnFallingBlock(location, material.createBlockData())
+                    fallingBlock.dropItem = false
+
+                    if (randomizer != 0.0)
+                        fallingBlock.velocity = Vector(
+                            velocity.x + Math.random() * (randomizer * 2) - randomizer,
+                            velocity.y + Math.random() * (randomizer * 2) - randomizer / 3,
+                            velocity.z + Math.random() * (randomizer * 2) - randomizer
+                        )
+                    else
+                        fallingBlock.velocity = velocity
+
+                    ShowUtils.addFallingBlock(fallingBlock)
+
+                    if (players != null && EffectMaster.isProtocolLibLoaded)
+                        for (player in Bukkit.getOnlinePlayers()) {
+                            if (!players.contains(player)) {
+                                val protocolManager = ProtocolLibrary.getProtocolManager()
+                                val removePacket = PacketContainer(PacketType.Play.Server.ENTITY_DESTROY)
+                                removePacket.intLists.write(0, listOf(fallingBlock.entityId))
+                                protocolManager.sendServerPacket(player, removePacket)
+                            }
+                        }
+                    location.add(x, y, z)
+                    c++
+                }
+            }.runTaskTimer(EffectMaster.plugin, 0L, 1L)
+        }catch (ex: IllegalArgumentException){
+            EffectMaster.plugin.logger.warning("Couldn't play effect with ID $id from ${getShow().getName()} in category ${getShow().getCategory()}.")
+            EffectMaster.plugin.logger.warning("The particle you entered doesn't exist. Please choose a valid type.")
+        }
     }
 
     override fun getType(): Type {
@@ -103,7 +120,7 @@ class FountainLine(effectShow: EffectShow, id: Int) : Effect(effectShow, id) {
         list.add(Pair("Velocity", "0, 0, 0"))
         list.add(Pair("Block", "BLUE_STAINED_GLASS"))
         list.add(Pair("Randomizer", 0))
-        list.add(Pair("Speed", 0.05))
+        list.add(Pair("Speed", 1))
         list.add(Pair("Delay", 0))
         return list
     }
