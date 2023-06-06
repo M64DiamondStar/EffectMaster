e16deacf063e9fc525c8b5ea39d9d7e96f3558bd
M64DiamondStar <59317305+M64DiamondStar@users.noreply.github.com>
2023-06-06 18:56:39 +0200

    Initial commit

---
+++ src/main/java/me/m64diamondstar/effectmaster/shows/type/ReplaceFill.kt
@@ -0,0 +1,94 @@
+package me.m64diamondstar.effectmaster.shows.type
+
+import me.m64diamondstar.effectmaster.EffectMaster
+import me.m64diamondstar.effectmaster.shows.utils.Effect
+import me.m64diamondstar.effectmaster.shows.utils.EffectShow
+import me.m64diamondstar.effectmaster.utils.LocationUtils
+import org.bukkit.Bukkit
+import org.bukkit.Location
+import org.bukkit.Material
+import org.bukkit.block.data.BlockData
+import org.bukkit.entity.Player
+
+class ReplaceFill(effectShow: EffectShow, private val id: Int) : Effect(effectShow, id) {
+
+    override fun execute(players: List<Player>?) {
+        try {
+            val fromLocation = LocationUtils.getLocationFromString(getSection().getString("FromLocation")!!) ?: return
+            val toLocation = LocationUtils.getLocationFromString(getSection().getString("ToLocation")!!) ?: return
+            val material =
+                if (getSection().get("Block") != null) Material.valueOf(
+                    getSection().getString("Block")!!.uppercase()
+                ) else Material.STONE
+            val duration = if (getSection().get("Duration") != null) getSection().getLong("Duration") else 0
+            val replacing = if (getSection().get("Replacing") != null) Material.valueOf(
+                getSection().getString("Replacing")!!.uppercase()
+            ) else Material.COBBLESTONE
+
+            val normalMap = HashMap<Location, BlockData>()
+
+            for (x in fromLocation.blockX.coerceAtLeast(toLocation.blockX) downTo toLocation.blockX.coerceAtMost(
+                fromLocation.blockX
+            )) {
+                for (y in fromLocation.blockY.coerceAtLeast(toLocation.blockY) downTo toLocation.blockY.coerceAtMost(
+                    fromLocation.blockY
+                )) {
+                    for (z in fromLocation.blockZ.coerceAtLeast(toLocation.blockZ) downTo toLocation.blockZ.coerceAtMost(
+                        fromLocation.blockZ
+                    )) {
+                        val location = Location(fromLocation.world, x.toDouble(), y.toDouble(), z.toDouble())
+
+                        // Check if the current location is the replacing block type
+                        if(location.block.type == replacing) {
+                            if (players != null) { // Send for specific players
+                                players.forEach {
+                                    it.sendBlockChange(location, material.createBlockData())
+                                }
+                            } else { // Send for all players
+                                for (player in Bukkit.getOnlinePlayers())
+                                    player.sendBlockChange(location, material.createBlockData())
+                            }
+                            normalMap[location] = location.block.blockData
+                        }
+                    }
+                }
+            }
+
+            Bukkit.getScheduler().scheduleSyncDelayedTask(EffectMaster.plugin, {
+                if (players != null) {
+                    players.forEach {
+                        for (loc in normalMap.keys)
+                            it.sendBlockChange(loc, normalMap[loc]!!)
+                    }
+                } else {
+                    for (player in Bukkit.getOnlinePlayers())
+                        for (loc in normalMap.keys)
+                            player.sendBlockChange(loc, normalMap[loc]!!)
+                }
+            }, duration)
+        } catch (ex: IllegalArgumentException) {
+            EffectMaster.plugin.logger.warning("Couldn't play effect with ID $id from ${getShow().getName()} in category ${getShow().getCategory()}.")
+            EffectMaster.plugin.logger.warning("The block you entered doesn't exist. Please choose a valid material.")
+        }
+    }
+
+    override fun getType(): Type {
+        return Type.REPLACE_FILL
+    }
+
+    override fun isSync(): Boolean {
+        return true
+    }
+
+    override fun getDefaults(): List<Pair<String, Any>> {
+        val list = ArrayList<Pair<String, Any>>()
+        list.add(Pair("Type", "REPLACE_FILL"))
+        list.add(Pair("FromLocation", "world, 0, 0, 0"))
+        list.add(Pair("ToLocation", "world, 3, 3, 3"))
+        list.add(Pair("Block", "STONE"))
+        list.add(Pair("Replacing", "COBBLESTONE"))
+        list.add(Pair("Duration", 100))
+        list.add(Pair("Delay", 0))
+        return list
+    }
+}
