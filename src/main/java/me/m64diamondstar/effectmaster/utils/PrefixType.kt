bde8293a07259283148ccad559aa4fb8c7aedc8e
M64DiamondStar <59317305+M64DiamondStar@users.noreply.github.com>
2023-07-16 18:14:31 +0200

    Customizable prefix

--- src/main/java/me/m64diamondstar/effectmaster/utils/PrefixType.kt
+++ src/main/java/me/m64diamondstar/effectmaster/utils/PrefixType.kt
@@ -1,11 +1,12 @@
 package me.m64diamondstar.effectmaster.utils
 
+import me.m64diamondstar.effectmaster.EffectMaster
+
 class Prefix {
 
     companion object {
-        val prefix = Colors.format("#c428fb&lE#c12af6&lf#be2df1&lf#bb2fec&le#b832e7&lc#b534e2&lt" +
-                " #b237dd&lM#af39d8&la#ac3cd3&ls#a93ece&lt#a641c9&le#a343c4&lr #858585» &f")
-        val shortPrefix = Colors.format("#c428fb&lE#c12af6&lM #858585» &f")
+        val prefix = EffectMaster.plugin.config.getString("messages.prefix.normal")!!
+        val shortPrefix = EffectMaster.plugin.config.getString("messages.prefix.short")!!
     }
 
     enum class PrefixType {
