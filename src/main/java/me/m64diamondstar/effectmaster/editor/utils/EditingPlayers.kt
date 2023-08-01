efe6b90bd9b18125b45b57fb81b8404ccaea87ca
M64DiamondStar <59317305+M64DiamondStar@users.noreply.github.com>
2023-08-01 11:08:04 +0200

    Changed to UUID instead of Player

--- src/main/java/me/m64diamondstar/effectmaster/editor/utils/EditingPlayers.kt
+++ src/main/java/me/m64diamondstar/effectmaster/editor/utils/EditingPlayers.kt
@@ -2,25 +2,26 @@ package me.m64diamondstar.effectmaster.editor.utils
 
 import me.m64diamondstar.effectmaster.shows.EffectShow
 import org.bukkit.entity.Player
+import java.util.UUID
 
 object EditingPlayers {
 
-    private val players = HashMap<Player, Triple<EffectShow, Int, String>>()
+    private val players = HashMap<UUID, Triple<EffectShow, Int, String>>()
 
     fun add(player: Player, effectShow: EffectShow, id: Int, parameter: String){
-        players[player] = Triple(effectShow, id, parameter)
+        players[player.uniqueId] = Triple(effectShow, id, parameter)
     }
 
     fun remove(player: Player){
-        players.remove(player)
+        players.remove(player.uniqueId)
     }
 
     fun get(player: Player): Triple<EffectShow, Int, String>? {
-        return players[player]
+        return players[player.uniqueId]
     }
 
     fun contains(player: Player): Boolean{
-        return players.contains(player)
+        return players.contains(player.uniqueId)
     }
 
 }
