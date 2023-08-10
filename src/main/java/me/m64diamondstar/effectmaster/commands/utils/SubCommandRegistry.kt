0c3e2e8bffbc5ceaeacae660f2d05928cd161759
M64DiamondStar <59317305+M64DiamondStar@users.noreply.github.com>
2023-08-10 21:48:01 +0200

    Added EditSubCommand.kt

--- src/main/java/me/m64diamondstar/effectmaster/commands/utils/SubCommandRegistry.kt
+++ src/main/java/me/m64diamondstar/effectmaster/commands/utils/SubCommandRegistry.kt
@@ -13,6 +13,7 @@ object SubCommandRegistry {
         SubCommandManager.registerSubCommand(PlaySubCommand())
         SubCommandManager.registerSubCommand(LocationSubCommand())
         SubCommandManager.registerSubCommand(WikiSubCommand())
+        SubCommandManager.registerSubCommand(EditSubCommand())
         SubCommandManager.registerSubCommand(EditorSubCommand())
         SubCommandManager.registerSubCommand(RenameSubCommand())
         SubCommandManager.registerSubCommand(PrivatePlaySubCommand())
