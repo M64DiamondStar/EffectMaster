ce3eab5bf5a28a778ced00a2c8a15c6b9b7bc096
M64DiamondStar <59317305+M64DiamondStar@users.noreply.github.com>
2023-08-12 09:45:45 +0200

    Made some changes so that prefix updates after plugin reload

--- src/main/java/me/m64diamondstar/effectmaster/utils/PrefixType.kt
+++ src/main/java/me/m64diamondstar/effectmaster/utils/PrefixType.kt
@@ -5,52 +5,55 @@ import me.m64diamondstar.effectmaster.EffectMaster
 class Prefix {
 
     companion object {
-        val prefix = EffectMaster.plugin.config.getString("messages.prefix.normal")!!
-        val shortPrefix = EffectMaster.plugin.config.getString("messages.prefix.short")!!
+        fun getPrefix(): String{
+            return EffectMaster.plugin.config.getString("messages.prefix.normal")!!
+        }
+
+        fun getShortPrefix(): String{
+            return EffectMaster.plugin.config.getString("messages.prefix.short")!!
+        }
     }
 
     enum class PrefixType {
         ERROR {
             override fun toString(): String {
-                return Colors.format("$prefix#bd4d4d")
+                return Colors.format("${getPrefix()}#bd4d4d")
             }
 
             override fun toShortString(): String {
-                return Colors.format("$shortPrefix#bd4d4d")
+                return Colors.format("${getShortPrefix()}#bd4d4d")
             }
         },
         SUCCESS {
             override fun toString(): String {
-                return Colors.format("$prefix#53bd4d")
+                return Colors.format("${getPrefix()}#53bd4d")
             }
 
             override fun toShortString(): String {
-                return Colors.format("$shortPrefix#53bd4d")
+                return Colors.format("${getShortPrefix()}#53bd4d")
             }
         },
         STANDARD {
             override fun toString(): String {
-                return Colors.format("$prefix#bfbfbf")
+                return Colors.format("${getPrefix()}#bfbfbf")
             }
 
             override fun toShortString(): String {
-                return Colors.format("$shortPrefix#bfbfbf")
+                return Colors.format("${getShortPrefix()}#bfbfbf")
             }
         },
         BACKGROUND {
             override fun toString(): String {
-                return Colors.format("$prefix#7d7d7d")
+                return Colors.format("${getPrefix()}#7d7d7d")
             }
 
             override fun toShortString(): String {
-                return Colors.format("$shortPrefix#7d7d7d")
+                return Colors.format("${getShortPrefix()}#7d7d7d")
             }
         };
 
         abstract override fun toString(): String
         abstract fun toShortString(): String
     }
-
-
 
 }
