588b723586e640173fd0594e92ee9f3f6ccd5274
M64DiamondStar <59317305+M64DiamondStar@users.noreply.github.com>
2024-08-12 18:12:10 +0200

    Now allows multiple pages

--- src/main/java/me/m64diamondstar/effectmaster/editor/effect/CreateEffectGui.kt
+++ src/main/java/me/m64diamondstar/effectmaster/editor/effect/CreateEffectGui.kt
@@ -1,5 +1,6 @@
 package me.m64diamondstar.effectmaster.editor.effect
 
+import me.m64diamondstar.effectmaster.editor.show.AllEffectsGui
 import me.m64diamondstar.effectmaster.editor.show.EditShowGui
 import me.m64diamondstar.effectmaster.editor.utils.EditorUtils
 import me.m64diamondstar.effectmaster.shows.EffectShow
@@ -14,7 +15,7 @@ import org.bukkit.event.inventory.InventoryClickEvent
 import org.bukkit.inventory.ItemFlag
 import org.bukkit.inventory.ItemStack
 
-class CreateEffectGui(private val player: Player, effectShow: EffectShow): Gui(player = player) {
+class CreateEffectGui(private val player: Player, effectShow: EffectShow, private val page: Int): Gui(player = player) {
 
     private val showCategory: String = effectShow.getCategory()
     private val showName: String = effectShow.getName()
@@ -30,9 +31,19 @@ class CreateEffectGui(private val player: Player, effectShow: EffectShow): Gui(p
     override fun handleInventory(event: InventoryClickEvent) {
         if(event.currentItem == null) return
 
+        if(event.slot == 41 && event.currentItem!!.type == Material.ARROW){
+            val effectShow = EffectShow(showCategory, showName, null)
+            val createEffectGui = CreateEffectGui(player, effectShow, page + 1)
+            createEffectGui.open()
+        }
         if(event.slot == 40){
             val editEffectShowGui = EditShowGui(event.whoClicked as Player, EffectShow(showCategory, showName, null))
             editEffectShowGui.open()
+        }
+        if(event.slot == 39 && event.currentItem!!.type == Material.ARROW){
+            val effectShow = EffectShow(showCategory, showName, null)
+            val createEffectGui = CreateEffectGui(player, effectShow, page - 1)
+            createEffectGui.open()
         }
 
         if(event.slot !in 9..35) return
@@ -57,30 +68,42 @@ class CreateEffectGui(private val player: Player, effectShow: EffectShow): Gui(p
         for(i in 36..44) inventory.setItem(i, GuiItems.getBlackPane())
 
         inventory.setItem(40, GuiItems.getBack())
+        if(Effect.Type.getAllEffects().size > 27 * (page + 1)){
+            inventory.setItem(41, GuiItems.getScrollFurther())
+        }
+        if(page > 0){
+            inventory.setItem(39, GuiItems.getScrollBack())
+        }
 
         val item = ItemStack(Material.STONE)
         val meta = item.itemMeta!!
-        Effect.Type.getAllEffects().forEach {
-            item.type = it.getDisplayMaterial()
-            meta.setDisplayName(Colors.format("#dcb5ff&l${it.getIdentifier().lowercase().replace("_", " ")
-                .replaceFirstChar(Char::titlecase)}"))
-            val description = it.getDescription()
-            val lore = mutableListOf<String>()
-            var line = ""
-            for(word in description.split(" ")){
-                if(line.length + word.length + 1 <= 40){
-                    line += if(line.isEmpty()) word else " $word"
-                }else{
-                    lore.add(Colors.format(Colors.Color.BACKGROUND.toString() + line.trim()))
-                    line = word
-                }
-            }
-            if(line.isNotEmpty()) lore.add(Colors.format(Colors.Color.BACKGROUND.toString() + line.trim()))
-            meta.lore = lore.toList()
-            meta.addItemFlags(ItemFlag.HIDE_POTION_EFFECTS)
 
-            item.itemMeta = meta
-            inventory.addItem(TypeData.setIdentifier(item, it.getIdentifier()))
+        if(Effect.Type.getAllEffects().size > page * 27){
+
+            for(i in page * 27 until Effect.Type.getAllEffects().size){
+                val effect = Effect.Type.getAllEffects()[i]
+                item.type = effect.getDisplayMaterial()
+                meta.setDisplayName(Colors.format("#dcb5ff&l${effect.getIdentifier().lowercase().replace("_", " ")
+                    .replaceFirstChar(Char::titlecase)}"))
+                val description = effect.getDescription()
+                val lore = mutableListOf<String>()
+                var line = ""
+                for(word in description.split(" ")){
+                    if(line.length + word.length + 1 <= 40){
+                        line += if(line.isEmpty()) word else " $word"
+                    }else{
+                        lore.add(Colors.format(Colors.Color.BACKGROUND.toString() + line.trim()))
+                        line = word
+                    }
+                }
+                if(line.isNotEmpty()) lore.add(Colors.format(Colors.Color.BACKGROUND.toString() + line.trim()))
+                meta.lore = lore.toList()
+                meta.addItemFlags(ItemFlag.HIDE_POTION_EFFECTS)
+
+                item.itemMeta = meta
+                inventory.addItem(TypeData.setIdentifier(item, effect.getIdentifier()))
+
+            }
         }
     }
 }
