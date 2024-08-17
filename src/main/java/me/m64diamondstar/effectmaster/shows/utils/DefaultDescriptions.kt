7c283c653c96feccc2511a511b332ee606b278e2
M64DiamondStar <59317305+M64DiamondStar@users.noreply.github.com>
2024-08-17 23:35:53 +0200

    Add default descriptions to avoid unnecessarily long strings

---
+++ src/main/java/me/m64diamondstar/effectmaster/shows/utils/DefaultDescriptions.kt
@@ -0,0 +1,11 @@
+package me.m64diamondstar.effectmaster.shows.utils
+
+object DefaultDescriptions {
+    const val DELAY = "The delay between the start of the show, and the start of this effect in minecraft ticks (1 second = 20 ticks)"
+    const val DURATION = "The duration of the effect in minecraft ticks (1 second = 20 ticks)."
+    const val LOCATION = "The location where the effect is going to be played."
+    const val BLOCK_DATA = "The block data to use on the block, use [] to leave it empty."
+    const val BLOCK = "The block to spawn."
+    const val VELOCITY = "The velocity of the effect."
+    const val PARTICLE = "The particle to spawn."
+}
