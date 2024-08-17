90862969806e24385d5dbd5dbc44768bc625b9b3
M64DiamondStar <59317305+M64DiamondStar@users.noreply.github.com>
2024-08-17 23:42:40 +0200

    Reload loops on reload command

--- src/main/java/me/m64diamondstar/effectmaster/commands/subcommands/ReloadSubCommand.kt
+++ src/main/java/me/m64diamondstar/effectmaster/commands/subcommands/ReloadSubCommand.kt
@@ -2,6 +2,9 @@ package me.m64diamondstar.effectmaster.commands.subcommands
 
 import me.m64diamondstar.effectmaster.EffectMaster
 import me.m64diamondstar.effectmaster.commands.utils.SubCommand
+import me.m64diamondstar.effectmaster.shows.EffectShow
+import me.m64diamondstar.effectmaster.shows.ShowLooper
+import me.m64diamondstar.effectmaster.shows.utils.ShowUtils
 import me.m64diamondstar.effectmaster.utils.Colors
 import me.m64diamondstar.effectmaster.utils.Prefix
 import org.bukkit.command.CommandSender
@@ -14,7 +17,13 @@ class ReloadSubCommand: SubCommand {
 
     override fun execute(sender: CommandSender, args: Array<String>) {
         EffectMaster.plugin().reloadConfig()
-        sender.sendMessage(Colors.format(Prefix.PrefixType.SUCCESS.toString() + "Successfully reloaded the config.yml."))
+        ShowUtils.getCategories().forEach { categoryFile ->
+            ShowUtils.getShows(categoryFile.name).forEach { showFile ->
+                val effectShow = EffectShow(categoryFile.name, showFile.nameWithoutExtension)
+                ShowLooper.updateLoop(effectShow)
+            }
+        }
+        sender.sendMessage(Colors.format(Prefix.PrefixType.SUCCESS.toString() + "Successfully reloaded the config.yml and show loops."))
     }
 
     override fun getTabCompleters(sender: CommandSender, args: Array<String>): ArrayList<String> {
