68fca19657449a30d28d518b4b17a619740786e2
M64DiamondStar <59317305+M64DiamondStar@users.noreply.github.com>
2024-08-17 23:35:17 +0200

    Updated gradle to kotlin

---
+++ build.gradle.kts
@@ -0,0 +1,82 @@
+import java.nio.charset.StandardCharsets
+
+plugins {
+    id("java")
+    id("java-library")
+    id("maven-publish")
+    id("org.jetbrains.kotlin.jvm") version "1.9.20"
+}
+
+val groupName = "me.M64DiamondStar"
+val artifactName = "EffectMaster"
+val pluginVersion = "1.4.1"
+
+group = groupName
+description = artifactName
+version = pluginVersion
+
+repositories {
+    mavenLocal()
+    maven {
+        url = uri("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
+    }
+    maven {
+        url = uri("https://oss.sonatype.org/content/groups/public/")
+    }
+    maven {
+        url = uri("https://ci.mg-dev.eu/plugin/repository/everything")
+    }
+    maven {
+        url = uri("https://repo.dmulloy2.net/repository/public/")
+    }
+    maven {
+        url = uri("https://repo.maven.apache.org/maven2/")
+    }
+}
+
+dependencies {
+    compileOnly("org.spigotmc:spigot-api:1.20.4-R0.1-SNAPSHOT")
+    compileOnly("com.bergerkiller.bukkit:TrainCarts:1.19.2-v1-SNAPSHOT")
+    compileOnly("com.comphenix.protocol:ProtocolLib:5.1.0")
+}
+
+val targetJavaVersion = 17
+java {
+    val javaVersion = JavaVersion.toVersion(targetJavaVersion)
+    if (JavaVersion.current() < javaVersion) {
+        toolchain.languageVersion.set(JavaLanguageVersion.of(targetJavaVersion))
+    }
+    withSourcesJar()
+    withJavadocJar()
+}
+
+tasks.withType<JavaCompile>().configureEach {
+    options.encoding = "UTF-8"
+
+    if (targetJavaVersion >= 10 || JavaVersion.current().isJava10Compatible) {
+        options.release.set(targetJavaVersion)
+    }
+}
+
+tasks.processResources {
+    filesMatching("plugin.yml") {
+        expand("pluginVersion" to pluginVersion)
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = groupName
+            artifactId = artifactName
+            version = pluginVersion
+
+            from(components["java"])
+        }
+    }
+}
+
+tasks.wrapper {
+    gradleVersion = "8.5"
+    distributionType = Wrapper.DistributionType.ALL
+}
