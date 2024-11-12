3eedf605fa3a9c9608750b6d8f552d16b089f33e
M64DiamondStar <59317305+M64DiamondStar@users.noreply.github.com>
2024-11-12 19:31:02 +0100

    Created new converter file meant to add extension functions

---
+++ src/main/java/me/m64diamondstar/effectmaster/utils/Converter.kt
@@ -0,0 +1,23 @@
+package me.m64diamondstar.effectmaster.utils
+
+import org.bukkit.Material
+
+/**
+ * This is used to convert a sequencer which features 3 dimensions and a material into a sequencer with just the dimensions.
+ * @return A map with 3 dimensions x, y and z for each index
+ */
+fun Map<Int, Quadruple<Double, Double, Double, Material?>>?.toTriple(): Map<Int, Triple<Double, Double, Double>>? {
+    return this?.mapValues { (_, value) ->
+        Triple(value.first, value.second, value.third)
+    }
+}
+
+/**
+ * This is used to convert a sequencer which features 2 dimensions and a material into a sequencer with just the dimensions.
+ * @return A map with 2 dimensions width and depth for each index
+ */
+fun Map<Int, Triple<Double, Double, Material?>>?.toPair(): Map<Int, kotlin.Pair<Double, Double>>? {
+    return this?.mapValues { (_, value) ->
+        kotlin.Pair(value.first, value.second)
+    }
+}
