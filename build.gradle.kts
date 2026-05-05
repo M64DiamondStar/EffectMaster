b0fe9d62ae2264e227964927f4ac1ee3c76333d2
M64DiamondStar <59317305+M64DiamondStar@users.noreply.github.com>
2026-05-05 12:35:59 +0200

    Updated version to beta 9

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 
 val groupName = "me.M64DiamondStar"
 val artifactName = "EffectMaster"
-val pluginVersion = "1.5.0-beta8"
+val pluginVersion = "1.5.0-beta9"
 
 group = groupName
 description = artifactName
